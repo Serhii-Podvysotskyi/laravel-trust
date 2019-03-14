0016faadf7d0c1d6190d63cb5839f68623ab565d
Serhii Podvysotskyi <podvysotskyi@hotmail.com>
2019-03-14 13:14:32 +0200

    update user observer

--- src/Observers/UserObserver.php
+++ src/Observers/UserObserver.php
@@ -3,20 +3,21 @@
 namespace Podvysotsky\Laravel\Trust\Observers;
 
 use Podvysotsky\Laravel\Trust\Facades\Trust;
+use Illuminate\Foundation\Auth\User;
 
 class UserObserver
 {
-    public function created($user)
+    public function created(User $user)
     {
         Trust::reload($user);
     }
 
-    public function updated($user)
+    public function updated(User $user)
     {
         Trust::reload($user);
     }
 
-    public function deleted($user)
+    public function deleted(User $user)
     {
         Trust::reload($user);
     }
