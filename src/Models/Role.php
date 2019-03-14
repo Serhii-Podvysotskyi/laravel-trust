e5a2233b0207580933df1570388c719c0102d231
Serhii Podvysotskyi <podvysotskyi@hotmail.com>
2019-03-14 13:10:45 +0200

    add get permissions for role model

--- src/Models/Role.php
+++ src/Models/Role.php
@@ -5,6 +5,7 @@ namespace Podvysotsky\Laravel\Trust\Models;
 use Illuminate\Foundation\Auth\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Podvysotsky\Laravel\Trust\Facades\Trust;
 
 /**
  * @property string $name
@@ -16,6 +17,11 @@ class Role extends Model
     protected $fillable = [
         'name'
     ];
+
+    public function getPermissions()
+    {
+        return Trust::getPermissions($this);
+    }
 
     public function permissions() {
         return $this->belongsToMany(Permission::class, 'role_permissions');
